b26044eb067168525605701b4d7a642ece169c3e
Aria <ariasadeghi@gmail.com>
2020-03-23 22:29:04 -0700

    missed colon

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
-  useNewUrlParserL true,
+  useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true
 });
