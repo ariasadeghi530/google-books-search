f20cbb17bac18110b0a076d2f94280e9c0e47594
Aria <ariasadeghi@gmail.com>
2020-03-24 00:42:54 -0700

    added google books api and useState and useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+require('dotenv').config();
+
+const [bookState, useBookState] = useState({
+  books: []
+})
 
 function App() {
+  useEffect(() => {
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyBZh-a4x_DWMuMSbODfA2Vh6fsls0dKA7E` )
+    .then((books) => console.log(books))
+    .catch(e => console.error(e))
+  })
   return (
     <>
+<div>Hello World</div>
     </>
   );
 }
